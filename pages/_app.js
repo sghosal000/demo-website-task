47af11b51719a83ff802746c6c94eb858423aabf
sghosal <sghosal@test.com>
2024-06-23 20:48:39 +0530

    converted useWindowWidth hook to contextAPI

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { WindowWidthProvider } from '../components/contexts/windowWidthContext';
 
 const App = ({ Component, pageProps }) => (
   <React.Fragment>
-    <Component {...pageProps} />
+    <WindowWidthProvider >
+      <Component {...pageProps} />
+    </WindowWidthProvider>
   </React.Fragment>
 );
 
